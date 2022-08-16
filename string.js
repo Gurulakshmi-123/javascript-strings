2d42627ece2208310fd52423330c9f02d7c306f2
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-16 19:20:13 +0530

    srtings concept

---
+++ string.js
@@ -0,0 +1,22 @@
+var x = "10";
+var y = "20";
+var z = x + y;
+console.log(z);
+
+
+var x = 10;
+var y = "20";
+var z = x + y;
+console.log(z);
+
+
+var x = 10;
+var y = 20;
+var z = "30";
+var result = x + y + z;
+console.log(result);
+
+var x = "100";
+var y = "10";
+var z = x / y;
+console.log(z);
